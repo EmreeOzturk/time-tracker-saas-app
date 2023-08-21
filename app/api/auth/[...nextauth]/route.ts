9740fad28c6fccc1d967f7f65423233b79b57c01
Emre Ozturk <emreeozturk42@gmail.com>
2023-08-21 06:25:10 +0300

    auth updated

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -3,29 +3,30 @@ import type { Adapter } from "next-auth/adapters";
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import prisma from "@/lib/prisma";
+// import { PrismaAdapter } from "@auth/prisma-adapter";
+// import prisma from "@/lib/prisma";
+
 const authOptions: NextAuthOptions = {
   session: {
     strategy: "jwt",
   },
   secret: process.env.SECRET,
-  adapter: PrismaAdapter(prisma) as Adapter,
+//   adapter: PrismaAdapter(prisma) as any,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        username: { label: "Username", type: "text", placeholder: "jsmith" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        return null;
-      },
-    }),
+    // CredentialsProvider({
+    //   name: "Credentials",
+    //   credentials: {
+    //     username: { label: "Username", type: "text", placeholder: "jsmith" },
+    //     password: { label: "Password", type: "password" },
+    //   },
+    //   async authorize(credentials) {
+    //     return null;
+    //   },
+    // }),
   ],
 };
 
